c745e80b289017884ee605e9a48a79ddb7371dba
Hannan Rhodes <Hannanrhodes@yahoo.com>
2015-05-03 18:18:44 -0400

    fixed bugs

--- src/protocol.h
+++ src/protocol.h
@@ -18,7 +18,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 68790 : 68791;
+    return testnet ? 69791 : 68791;
 }
 
 
